8c3e88877a3f3cbd8181b257f24aebf73d9bcf19
Divine Dube <216004854@student.uj.ac.za>
2018-03-31 02:11:45 +0200

    init commit tracker

---
+++ app/src/main/java/com/dubedivine/tracker/service/MainControlService.kt
@@ -0,0 +1,121 @@
+package com.dubedivine.tracker.service
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.view.View
+import android.view.WindowManager
+import android.view.Gravity
+import android.graphics.PixelFormat
+import android.view.LayoutInflater
+import android.widget.ImageView
+import com.dubedivine.tracker.R
+import android.R.attr.y
+import android.R.attr.x
+import android.view.MotionEvent
+
+
+class MainControlService() : Service() {
+
+    private lateinit var mWindowManager: WindowManager
+    private lateinit var mMainControlView: View
+
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //inflate our XML view
+        mMainControlView = LayoutInflater.from(this).inflate(R.layout.layout_main_control_head, null)
+
+        //Add the view to the window.
+        val params = WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT)
+
+        //Specify the chat head position
+        //Initially view will be added to top-left corner
+        params.gravity = Gravity.TOP or Gravity.LEFT
+        params.x = 0
+        params.y = 100
+
+        //Add the view to the window
+        mWindowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        mWindowManager.addView(mMainControlView, params)
+
+        //Set the close button.
+        val closeButton = mMainControlView.findViewById(R.id.close_btn) as ImageView
+        closeButton.setOnClickListener({
+            //close the service and remove the chat head from the window
+            stopSelf()
+        })
+
+
+//Drag and move chat head using user's touch action.
+        val chatHeadImage = mMainControlView.findViewById(R.id.chat_head_profile_iv) as ImageView
+        chatHeadImage.setOnTouchListener(object : View.OnTouchListener {
+            private var lastAction: Int = 0
+            private var initialX: Int = 0
+            private var initialY: Int = 0
+            private var initialTouchX: Float = 0.toFloat()
+            private var initialTouchY: Float = 0.toFloat()
+
+            override fun onTouch(v: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+
+                        //remember the initial position.
+                        initialX = params.x
+                        initialY = params.y
+
+                        //get the touch location
+                        initialTouchX = event.rawX
+                        initialTouchY = event.rawY
+
+                        lastAction = event.action
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        //As we implemented on touch listener with ACTION_MOVE,
+                        //we have to check if the previous action was ACTION_DOWN
+                        //to identify if the user clicked the view or not.
+                        if (lastAction == MotionEvent.ACTION_DOWN) {
+                            //Open the chat conversation click.
+                            val intent = Intent(this@MainControlService, MainControlService::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+
+                            //close the service and remove the chat heads
+                            stopSelf()
+                        }
+                        lastAction = event.action
+                        return true
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        //Calculate the X and Y coordinates of the view.
+                        params.x = initialX + (event.rawX - initialTouchX).toInt()
+                        params.y = initialY + (event.rawY - initialTouchY).toInt()
+
+                        //Update the layout with new X & Y coordinate
+                        mWindowManager.updateViewLayout(mMainControlView, params)
+                        lastAction = event.action
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mMainControlView != null) mWindowManager.removeView(mMainControlView)
+    }
+}
